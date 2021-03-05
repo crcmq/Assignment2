24e675378be69f25a5aa0d5c1ca039b7e1a9386b
Roger Chen <yanmiao1992@hotmail.com>
2021-03-05 14:51:17 -0700

    Create data field of reservation and flight

--- src/sait/frms/problemdomain/Flight.java
+++ src/sait/frms/problemdomain/Flight.java
@@ -1,5 +1,16 @@
 package sait.frms.problemdomain;
 
 public class Flight {
-
+	private String code;
+	private String airlineName;
+	private String from;
+	private String to;
+	private String weekday;
+	private String time;
+	private int seats;
+	private double costPerSeat;
+	
+	public Flight() {
+		
+	}
 }
--- src/sait/frms/problemdomain/Reservation.java
+++ src/sait/frms/problemdomain/Reservation.java
@@ -1,5 +1,15 @@
 package sait.frms.problemdomain;
 
 public class Reservation {
-
+	private String code;
+	private String flightCode;
+	private String airline;
+	private String name;
+	private String citizenship;
+	private double cost;
+	private boolean active;
+	
+	public Reservation() {
+		
+	}
 }
