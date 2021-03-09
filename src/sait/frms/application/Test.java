427784150c13f4ec0976c11965c2317574a01fd9
Roger Chen <yanmiao1992@hotmail.com>
2021-03-09 13:48:57 -0700

    Test on Flight

--- src/sait/frms/application/Test.java
+++ src/sait/frms/application/Test.java
@@ -1,4 +1,7 @@
 package sait.frms.application;
+import java.util.ArrayList;
+
+import sait.frms.exception.EmptyFlightException;
 import sait.frms.problemdomain.*;
 /**
  * This is a test class. Should be deleted after the project is completed.
@@ -7,15 +10,19 @@ import sait.frms.problemdomain.*;
  */
 public class Test {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws EmptyFlightException {
+		ArrayList<Flight> flights = new ArrayList<>();
+		try {
+			Flight f = new Flight("CA-1234", "YYC", "YVR", "Monday", "17:00", 200, 100);
+			flights.add(f);
+		}
+		catch(EmptyFlightException e) {
+			
+		}
 		
-		Flight f = new Flight("CA-1234", "YYC", "YVR", "Monday", "17:00", 200, 100);
-		System.out.println(f.toString());
-		System.out.println(f.getAirline());
-		System.out.println(f.isDomestic());
+		System.out.println(flights.get(0).toString());
 		
-		Reservation r = new Reservation("D1234", "CA-1234", "CONN AIR", "Roger", "Canada", 100, true);
-		System.out.println(r.toString());
+		
 	}
 
 }
