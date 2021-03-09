85817b186315bc882bd1dd4d50be5fd3b2ff6a27
Roger Chen <yanmiao1992@hotmail.com>
2021-03-09 13:47:30 -0700

    Comment on InvalidRservationCodeException

--- src/sait/frms/exception/InvalidReservationCodeException.java
+++ src/sait/frms/exception/InvalidReservationCodeException.java
@@ -1,5 +1,10 @@
 package sait.frms.exception;
 
+/**
+ * If reservation code is invalid, throw this exception
+ * @author Mengqiu (Roger) Chen
+ *
+ */
 public class InvalidReservationCodeException extends Exception{
 	public InvalidReservationCodeException () {
 	}
