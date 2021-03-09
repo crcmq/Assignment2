d314c8e378c547402900e0fd219cb78b675c65e4
Roger Chen <yanmiao1992@hotmail.com>
2021-03-09 13:47:52 -0700

    Comment on InvalidFlightCodeException

--- src/sait/frms/exception/InvalidFlightCodeException.java
+++ src/sait/frms/exception/InvalidFlightCodeException.java
@@ -1,5 +1,10 @@
 package sait.frms.exception;
 
+/**
+ * If flight code is invalid, throw this excception
+ * @author Mengqiu (Roger) Chen
+ *
+ */
 public class InvalidFlightCodeException extends Exception{
 	public InvalidFlightCodeException() {
 	}
