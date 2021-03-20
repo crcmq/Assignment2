2648d110beeade3fc4bee242e676a6205834244e
Roger Chen <yanmiao1992@hotmail.com>
2021-03-19 19:06:55 -0600

    Add throws exception

--- src/sait/frms/application/AppDriver.java
+++ src/sait/frms/application/AppDriver.java
@@ -1,6 +1,8 @@
 package sait.frms.application;
 
 
+import java.io.FileNotFoundException;
+
 import sait.frms.gui.*;
 
 /**
@@ -13,8 +15,9 @@ public class AppDriver
 	/**
 	 * Entry point to Java application.
 	 * @param args
+	 * @throws FileNotFoundException 
 	 */
-	public static void main(String[] args) 
+	public static void main(String[] args) throws FileNotFoundException 
 	{
 		MainWindow mainWindow = new MainWindow();
 		mainWindow.display();
