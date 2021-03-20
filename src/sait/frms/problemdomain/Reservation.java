5d738ecc02d88cea75c862eeaaf194e0dab8a84f
Roger Chen <yanmiao1992@hotmail.com>
2021-03-20 16:27:43 -0600

    write comments

--- src/sait/frms/problemdomain/Reservation.java
+++ src/sait/frms/problemdomain/Reservation.java
@@ -1,5 +1,13 @@
 package sait.frms.problemdomain;
 
+import sait.frms.exception.NullCitizenshipException;
+import sait.frms.exception.NullClientNameException;
+
+/**
+ * 
+ * @author 845368
+ *
+ */
 public class Reservation {
 	private String code;
 	private String flightCode;
@@ -22,42 +30,90 @@ public class Reservation {
 		this.active = _active;
 	}
 
+	/**
+	 * Get reservation code 
+	 * @return reservation code
+	 */
 	public String getCode() {
 		return code;
 	}
 
+	/**
+	 * Get flight code
+	 * @return flight code
+	 */
 	public String getFlightCode() {
 		return flightCode;
 	}
 
+	/**
+	 * Get airline name
+	 * @return the name of airline
+	 */
 	public String getAirline() {
 		return airline;
 	}
 
+	/**
+	 * Get client name
+	 * @return client name
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * Get client citizenship
+	 * @return client citizenship
+	 */
 	public String getCitizenship() {
 		return citizenship;
 	}
 
+	/**
+	 * 
+	 * @return the cost of the reservation
+	 */
 	public double getCost() {
 		return cost;
 	}
 
+	/**
+	 * 
+	 * @return the status of this reservation: active / inactive
+	 */
 	public boolean isActive() {
 		return active;
 	}
 	
-	public void setName(String _name) {
+	/**
+	 * Change client name of reservation
+	 * @param _name
+	 * @throws NullClientNameException Thrown if name is empty
+	 */
+	public void setName(String _name) throws NullClientNameException {
+		if (_name.equals("")) {
+			throw new NullClientNameException();
+		}
 		this.name = _name;
 	}
 	
-	public void setCitizenship (String _citizenship) {
+	/**
+	 * Change client citizenship of reservation
+	 * @param _citizenship
+	 * @throws NullCitizenshipException Thrown if citizenship is empty
+	 */
+	public void setCitizenship (String _citizenship) throws NullCitizenshipException {
+		if (_citizenship.equals("")) {
+			throw new NullCitizenshipException();
+		}
 		this.citizenship = _citizenship;
 	}
 	
+	/**
+	 * Change status of reservation
+	 * @param _active
+	 */
 	public void setActive (boolean _active) {
 		this.active = _active;
 	}
