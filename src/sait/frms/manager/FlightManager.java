21bec086f5c42cac7abba996cd4bdbfd7b1582e5
Roger Chen <yanmiao1992@hotmail.com>
2021-03-22 18:59:23 -0600

    Update FlightManager.java

--- src/sait/frms/manager/FlightManager.java
+++ src/sait/frms/manager/FlightManager.java
@@ -4,6 +4,14 @@ import sait.frms.problemdomain.*;
 import java.util.*;
 import java.io.*;
 
+/**
+ * Flight manager to manage all the flights
+ * The data of flights come from "res/flights.csv"
+ * The data of airports come from "res/airports.csv"
+ *  
+ * @author Mengqiu (Roger) Chen, Ebele Egenti, AJ Russell De Leon, Dmitriy Fominykh
+ *
+ */
 public class FlightManager {
 	public static final String WEEKDAY_ANY = "Any";
 	public static final String WEEKDAY_SUNDAY = "Sunday";
@@ -18,15 +26,27 @@ public class FlightManager {
 	private ArrayList<String> airports = new ArrayList<>();
 	private ArrayList<String> airportNames = new ArrayList<>();
 	
+	/**
+	 * Constructor loading all the flights and airports
+	 * @throws FileNotFoundException Thrown if flights.csv or airports.csv not found
+	 */
 	public FlightManager() throws FileNotFoundException {
 		populateFlights();
 		populateAirports();
 	}
 	
+	/**
+	 * Get list of airports
+	 * @return airports ArrayList of airports
+	 */
 	public ArrayList<String> getAirports() {
 		return this.airports;
 	}
 	
+	/**
+	 * Get list of flights
+	 * @return flights ArrayList of flights
+	 */
 	public ArrayList<Flight> getFlights () {
 		return this.flights;
 	}
@@ -52,10 +72,11 @@ public class FlightManager {
 	}
 	
 	/**
-	 * Find out the flight according to the given code.
-	 * @param code
+	 * Find out the flight according to the given flight code.
+	 * 
+	 * @param code Flight code
 	 * @return result The flight found according to the code
-	 * @throws FlightDoesNotExist
+	 * 
 	 */
 	public Flight findFlightByCode(String code) {
 		Flight result = new Flight();
@@ -66,15 +87,14 @@ public class FlightManager {
 				result = f;
 			}
 		}
-
 		return result;
 	}
 	
 	/**
-	 * Check all the flights to see if there is a flight matching the condition. If so, add it to the list
-	 * @param from
-	 * @param to
-	 * @param weekday
+	 * Check all the flights to see if there is a flight matching the condition. If so, add it to the list of found flights
+	 * @param from From airport
+	 * @param to To airport
+	 * @param weekday Weekday of the flight
 	 * @return flights List of flights matching condition
 	 */
 	public ArrayList<Flight> findFlights(String from, String to, String weekday) {
@@ -95,8 +115,7 @@ public class FlightManager {
 	
 	/**
 	 * Add flights from csv file
-	 * @throws FileNotFoundException 
-	 * @throws EmptyFlightException 
+	 * @throws FileNotFoundException Thrown if flights.csv not found
 	 */
 	private void populateFlights() throws FileNotFoundException {
 		File file = new File ("res\\flights.csv");
@@ -122,7 +141,6 @@ public class FlightManager {
 			catch (InvalidFlightCodeException e) {
 				// skip adding this flight
 			}
-
 		}
 		in.close();
 	}
@@ -144,10 +162,10 @@ public class FlightManager {
 				airports.add(airport[0]);
 				airportNames.add(airport[1]);
 			}
+			in.close();
 		} 
 		catch (FileNotFoundException e) {
 			System.out.println("airports.csv does not exist");
 		}
-		
 	}
 }
