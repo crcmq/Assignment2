7f1f0e251eccbc6655da9b30f984cf09b7711b34
Roger Chen <yanmiao1992@hotmail.com>
2021-03-22 18:58:18 -0600

    Update NullCitizenshipException.java

--- src/sait/frms/exception/NullCitizenshipException.java
+++ src/sait/frms/exception/NullCitizenshipException.java
@@ -2,7 +2,7 @@ package sait.frms.exception;
 
 /**
  * This exception is thrown if citizenship is empty
- * @author Mengqiu (Roger) Chen
+ * @author Mengqiu (Roger) Chen, Ebele Egenti, AJ Russell De Leon, Dmitriy Fominykh
  *
  */
 public class NullCitizenshipException extends Exception {
