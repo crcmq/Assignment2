36ee084a2e2c5f145678be5fa005ca32465b4244
Roger Chen <yanmiao1992@hotmail.com>
2021-03-22 18:58:28 -0600

    Update SeatUnavailableException.java

--- src/sait/frms/exception/SeatUnavailableException.java
+++ src/sait/frms/exception/SeatUnavailableException.java
@@ -1,8 +1,8 @@
 package sait.frms.exception;
 
 /**
- * If the seat of fligt is 0, throw this exception
- * @author Mengqiu (Roger) Chen
+ * If the seat of flight is 0, and user still wants to book this flight, then throw this exception
+ * @author Mengqiu (Roger) Chen, Ebele Egenti, AJ Russell De Leon, Dmitriy Fominykh
  *
  */
 public class SeatUnavailableException extends Exception{
